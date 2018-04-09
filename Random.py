61ea4a34bf8b722c579833d19690278c121ac3fd
xDaxHarvesterx <38172334+xDaxHarvesterx@users.noreply.github.com>
2018-04-09 17:26:30 -0600

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from random import randint
+
+print("This is the randomizer project!\nCheck this out!")
+
+Running = True
+
+try:
+    while Running:
+	print(randint(0,2048506209573))
+
+except KeyboardInterrupt:
+    print("Im quitting!")
